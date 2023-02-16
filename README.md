3f7d4f9c15b62511e8eb2356e1191c8173811bb8
Timothy Molina <60799704+Superboy20@users.noreply.github.com>
2023-02-15 19:47:35 -0500

    Project Completed for Stevens

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # PECodingExercise
 Coding Exercise for Platform Engineer position
+
+
+Hello, my name is Tim Molina and here is my project. I just made a simple site that fell within the parameters of the prompt.
+I did however spend a little longer than three hours on it, mainly on styling and ironing out a few bugs that I encountered, but nothing too bad I suppose!
+Another extra thing I did was that I made it accessible to mobile users with an emphasis on responsive design, however I do suspect certain tablets would
+look quite a bit "off" to say the least.
+
+As for what excites me about the opportunity to work at Stevens, I have to admit that everything about it is very attractive to me, from the culture, to the job that I would hypothetically be doing.
+I want to work at Stevens as a Platform Engineer because it is a job that I know I can be successful at. I have the skills required as listed in the job description, which can be found
+in my resume and portfolio. But to be specific, I know React, JavaScript, CSS/HTML, and a little MySQL, although PostgreSQL is what I have more experience in, however;
+it is not hard for me to pick any technology up. This is not only my career, but also my hobby, as I also have knowledge in a few other skills that are not listed in the description,
+specifically my favorite which is C# as I enjoy making video games in my off time with other specific technologies.
+
+I am a creator and technophile at my core, and although I am still early on in my career, I desire for nothing more than to get better at what I do.
+I believe that Stevens can provide for me the environment to refine my skills and grow in my career while also being able to contribute to something that I admire!
---
+++ app.js
@@ -0,0 +1,118 @@
+const people = document.getElementById("cards-wrap");
+const peopleNext = document.getElementById("people_next");
+const peoplePrevious = document.getElementById("people_previous");
+
+let peopleURL = "https://swapi.dev/api/people/?page=1";
+let nextPeoplePage;
+let previousPeoplePage;
+
+// nextPage and previousPage functions that grab the next/previous page based on the
+// swapi keywords to grab the necessary data because swapi only gives 10 results per page
+function nextPage() {
+    if (nextPeople) {
+        peopleURL = new URL(nextPeople);
+    }
+    fetchNames()
+}
+
+function previousPage() {
+    if (previousPeople) {
+        peopleURL = new URL(previousPeople);
+    }
+    fetchNames()
+}
+
+// Event listeners for buttons to navigate between next and previous pages
+// using the functions nextPage() and previousPage()
+if (peopleNext) {
+    peopleNext.addEventListener("click", nextPage);
+}
+
+if (peoplePrevious) {
+    peoplePrevious.addEventListener("click", previousPage);
+}
+
+// Function that fetches the data from the peopleURL that holds the API URL
+async function fetchNames() {
+
+    // On non-mobile view, shows a darkened screen with the word "Loading" as the data is being fetched
+    document.querySelector('.overlay').classList.add('active');
+    let results = await fetch(peopleURL);
+    const data = await results.json();
+    nextPeople = data.next;
+    previousPeople = data.previous;
+    let characters = data.results;
+
+    // Output is set to an empty string before the loop, as it will paste the info dynamically as it is filled
+    let output = '';
+
+    // Loop through the characters array, and sets the item var to represent each character
+    for (let i = 0; i < characters.length; i++) {
+        let item = characters[i];
+
+        // Vehicle names, manufactuerers, and worth are all set to an empty array
+        let vehicleNames = [];
+        let vehicleManufacturers = [];
+        let vehicleWorth = [];
+
+        // If a character is NOT associated with any vehicles, provide 'N/A' in the text content
+        if (item.vehicles.length === 0) {
+            vehicleNames.push('N/A');
+            vehicleManufacturers.push('N/A');
+            vehicleWorth.push('N/A');
+        } else {
+            // Loop through each character's vehicle info, push results into vehicleNames, vehicleManufacturers, vehicleWorth arrays
+            // with the necessary info
+            for (let j = 0; j < item.vehicles.length; j++) {
+                let vehicleResult = await fetch(item.vehicles[j]);
+                let vehicleData = await vehicleResult.json();
+                vehicleNames.push(vehicleData.name);
+                vehicleManufacturers.push(vehicleData.manufacturer);
+                vehicleWorth.push(vehicleData.cost_in_credits);
+            }
+        }
+        // Paste info dynamically into HTML with the necessary content filled out
+        output += `<div class="card-item">
+            <div class="card-inner">
+              <div class="character-info">
+              <button class="collapsible"> ${item.name}</button>
+              </div>
+              <div id="empty" class="content character-info">
+              <p>Vehicle Name: ${vehicleNames.join(', ')}<p>
+              <p>Vehicle Manufacturer: ${vehicleManufacturers.join(', ')}<p>
+              <p> Vehicle Cost: ${vehicleWorth.join(', ')}<p>
+              </div>
+
+              <div class="character-info">Height: ${item.height} cm</div>
+              <div class="character-info">Weight: ${item.mass} kg</div>
+              <div class="character-info">Birth Year: ${item.birth_year}</div>
+              <div class="character-info">Gender: ${item.gender}</div>
+              <div class="character-info">Hair Color: ${item.hair_color}</div>
+              </div>
+              </div>`
+    }
+
+    // Remove the darkend loading page after data is fetched and displayed
+    document.querySelector('.overlay').classList.remove('active');
+    people.innerHTML = output;
+    showVehicles();
+}
+
+// Toggle function that hides/displays vehicle info based on the character name clicked
+// On page load, it checks if the display is set to block, if so, set it to none.
+function showVehicles() {
+    let collection = document.getElementsByClassName("collapsible");
+    let content = document.getElementsByClassName("content");
+    for (let i = 0; i < collection.length; i++) {
+        collection[i].addEventListener("click", function () {
+            this.classList.toggle("active");
+            if (content[i].style.display === "block") {
+                content[i].style.display = "none";
+            } else {
+                content[i].style.display = "block";
+            }
+        });
+    }
+}
+
+fetchNames();
